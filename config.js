bfb3560a1d22d12a0e38c0bf1d112d60c1932766
Martreis <manuelareisadv2013@gmail.com>
2026-09-22 15:15:28 -0300

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const VIBRA_CONFIG = {
-    SUPABASE_URL: "COLE_AQUI_SUA_URL",
-    SUPABASE_PUBLISHABLE_KEY: "COLE_AQUI_SUA_CHAVE_PUBLICA"
+    SUPABASE_URL: https://dkkudlzpopmfhyqwjfsv.supabase.co,
+    SUPABASE_PUBLISHABLE_KEY: sb_publishable_r1YEZ_TDBIJsfZUKfVTLPQ_y2pTX6re
 };
 
 window.VIBRA_CONFIG = VIBRA_CONFIG;
